b40c8be34fbddd32346aa5eb39a38d774260d3dc
Cem Ersoz <cersozz@gmail.com>
2017-09-18 13:04:02 -0400

    refactoring - start

--- cuneyd_bot.py
+++ cuneyd_bot.py
@@ -2,13 +2,15 @@ import math
 import random
 import sensor
 class CuneydBot(object):
-    def __init__(self,x,y,th,id,p = 1,sensors_xyt = [(-1,1,math.pi),(1,1,0),(0,1,math.pi/2)]):
-        self.x = x
-        self.y = y
-        self.th = th
+    def __init__(self, x, y, th, vl=0, vr=0, id=0,p = 1,sensors_xyt = [(-1,1,math.pi),(1,1,0),(0,1,math.pi/2)],gr=100):
+	self.pos = [x,y,th]
+	self.wheel_vel = [vl,vr]
+	self.sensed_pos = [x,y,th]
         self.ID = ID
         self.p = p
         self.sensors = []
+	self.gr=gr
+	
     def diff_drive(self,v,w):
         x = math.sin(w*t)*v*t
         y = math.cos(w*t)*v*t
@@ -23,15 +25,18 @@ class CuneydBot(object):
         self.sensors.append(Sensor.sensor_type(x,y,th))
         pass
 
-    def diff_drive_overtime(self,t,dt,v,w):
+    def diff_drive_overtime(self,t):
         #calculates the robots position with respect to itself after t seconds,
         #with dt in miliseconds, with the selected drive method and updates it
         #relative to cardinal coordinates
-        x,y = 0
-        for i in range(0,int(t*1000/dt)):
-            dx,dy = diff_drive(v,w)
-            x += dx
-            y += dy
+	W = self.wheel_vel[1] - self.wheel_vel[0]
+	V = sum(self.wheel_vel)/2
+        x,y,th = 0
+        for dt in range(0,t, 1.0/self.gr):
+            th += dt * W / 2
+            x += math.cos(th)*V*dt
+            y += math.sin(th)*V*dt
+            th += dt * W / 2	
         #update coordinates
         self.x = x*math.cos(self,th) - y*math.sin(self.th)
         self.y = x*math.sin(self.th) + y*math.cos(self,th) 
